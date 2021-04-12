25dbf331f6e5874a88830c3cb1502cbffaeaea7d
NoahSablan <78834803+NoahSablan@users.noreply.github.com>
2021-04-12 16:15:30 -0400

    Added changesize, and pointerselection
    Attach changesize to each new object, it will be activated when the object is selected, and disabled when the object is not selected. Pointerselection is a script that is attached to a pointer attached to the left hand, something I haven't done yet was to attach the pointer script directly to the oculus controller, but for now it is on a rectangle that is visible on the left hand for testing. 
    
    BUTTONS: LEFT TRIGGER, RIGHT TRIGGER, SQUEEZE LEFT, SQUEEZE RIGHT.

---
+++ Group 5 Final Project/Assets/Scripts/ChangeSize.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSize : MonoBehaviour
+{
+	public GameObject obj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	if (OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger) > 0.0f) {
+
+    		//LOCK UNIFORMITY
+    		if( OVRInput.Get(OVRInput.Axis1D.SecondaryHandTrigger) > 0.0f) { 
+    			Vector3 uniformScale = new Vector3(OVRInput.GetLocalControllerVelocity(OVRInput.Controller.RHand).x,
+    												 OVRInput.GetLocalControllerVelocity(OVRInput.Controller.RHand).x,
+    												 OVRInput.GetLocalControllerVelocity(OVRInput.Controller.RHand).x);
+    			obj.transform.localScale += uniformScale * 0.5f;
+    		}
+
+    		else {
+
+    			obj.transform.localScale += OVRInput.GetLocalControllerVelocity(OVRInput.Controller.RHand) * 0.5f;
+    		}
+    		Debug.Log(OVRInput.GetLocalControllerVelocity(OVRInput.GetActiveController()));
+    		Debug.Log("inIF");
+    	}
+
+    	if (OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger) > 0.0f) {
+    		//LOCK UNIFORMITY
+    		if( OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger) > 0.0f) { 
+    			Vector3 uniformScale = new Vector3(OVRInput.GetLocalControllerVelocity(OVRInput.Controller.LHand).x,
+    												 OVRInput.GetLocalControllerVelocity(OVRInput.Controller.LHand).x,
+    												 OVRInput.GetLocalControllerVelocity(OVRInput.Controller.LHand).x);
+    			obj.transform.localScale += uniformScale * 0.5f;
+    		}
+
+    		else {
+    			
+    			obj.transform.localScale += OVRInput.GetLocalControllerVelocity(OVRInput.Controller.LHand) * 0.5f;
+    		}
+
+    	}
+
+    }
+}
---
+++ Group 5 Final Project/Assets/Scripts/PointerSelection.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointerSelection : MonoBehaviour
+{
+	//This is represented by the cube at the tip of the wand
+  public GameObject pointer;
+
+  private GameObject selection = null;
+
+	// public GameObject selected;
+
+	// private bool collision;
+
+
+    // Start is called before the first frame update
+    // void Start()
+    // {
+        
+    // }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	RaycastHit hit;
+    	Outline o;
+        ChangeSize c;
+
+        if (Physics.Raycast(pointer.transform.position, pointer.transform.TransformDirection(Vector3.forward), out hit)){
+    		// Debug.DrawRay(pointer.transform.position, pointer.transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+      //   	Debug.Log(hit.collider.gameObject.name);
+     		Debug.Log("Ray Hit something. It was");
+        	Debug.Log(hit.collider.gameObject.name);
+
+
+            // if the previous selection was not the current selection, unmark it
+            if ( (selection != null) && (selection.name != hit.collider.gameObject.name)) {
+ 
+                o = selection.gameObject.GetComponent<Outline>();   
+                o.enabled = false;
+
+                c = selection.gameObject.GetComponent<ChangeSize>();
+                c.enabled = false;
+
+            }
+
+            // add a tag to the hit "UnitSelected", this will indicate the selection, and should be
+            // checked for if you want to know if an object is selected.
+
+            selection = hit.collider.gameObject;
+            o = selection.gameObject.GetComponent<Outline>();
+            o.enabled = true;
+
+            c = selection.gameObject.GetComponent<ChangeSize>();
+            c.enabled = true;
+            Debug.Log(o.enabled);
+            Debug.Log(!o.enabled);
+            // hit.collider.gameObject.GetComponent.enabled = true;
+            selection = hit.collider.gameObject;
+
+
+
+    	}
+	}
+}
+
